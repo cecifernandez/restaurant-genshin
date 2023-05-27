facb5c66eaa828b37276ac0065c94ef32cf8a9fc
cecifernandez <mceciliafernandezst@gmail.com>
2023-05-27 18:43:14 -0300

    change

--- src/loadWebsite.js
+++ src/loadWebsite.js
@@ -1,11 +1,9 @@
-import loadContact from "./contact";
 import loadHome from "./home";
-import loadMenu from "./menu";
 import loadNav from "./nav";
 
 function loadPage() {
   loadNav();
-  loadContact();
+  loadHome();
 }
 
 export default loadPage;
